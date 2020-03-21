6119a393e22822b58a20c6b35da3e278383e0fa2
Deyvison Gomes <tutacf@gmail.com>
2020-03-20 21:50:20 -0300

    Implementing simple search

--- web_pc/Models/SalesRecord.cs
+++ web_pc/Models/SalesRecord.cs
@@ -3,12 +3,14 @@ using web_pc.Models.Enums;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace web_pc.Models
 {
     public class SalesRecord
     {
         public int Id            { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date     { get; set; }
         public double Amount     { get; set; }
         public SaleStatus Status { get; set; }
---
+++ web_pc/Service/SalesRecordService.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_pc.Data;
+using web_pc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace web_pc.Service
+{
+    public class SalesRecordService
+    {
+        private readonly web_pcContext _context;
+
+        public SalesRecordService(web_pcContext context) { _context = context; }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if(maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
